e647ea43678e47fa715f3523609e7b242b639035
Praise25 <anenepraise1@gmail.com>
2022-12-04 18:10:33 +0100

    chore: complete tasks

--- task.py
+++ task.py
@@ -1,5 +1,8 @@
 import math
+import psycopg2
+import random
 from bs4 import BeautifulSoup
+from config import config
 
 
 def extract_colors():
@@ -28,6 +31,11 @@ def create_color_dictionary(color_list):
     return color_dict
 
 
+#################################################################################################
+################################# Task Solutions ################################################
+#################################################################################################
+
+# Solution to task 1
 def get_mean_color(data, frequency_data):
     num_of_colors_types = len(frequency_data)
     total_num_of_colors = sum(frequency_data.values())
@@ -36,6 +44,7 @@ def get_mean_color(data, frequency_data):
     return mean_color
 
 
+# Solution to task 2
 def get_mode_color(frequency_data):
     mode_frequency = max(frequency_data.values())
     mode_color = ""
@@ -45,12 +54,14 @@ def get_mode_color(frequency_data):
     return mode_color
 
 
+# Solution to task 3
 def get_median_color(data):
     median_index = (len(data) + 1) // 2
     median_color = data[median_index]
     return median_color
 
 
+# Solution to task 4
 def get_variance(frequency_data):
     num_of_color_types = len(frequency_data)
     total_number_of_colors = sum(frequency_data.values())
@@ -62,6 +73,7 @@ def get_variance(frequency_data):
     return variance
 
 
+# Solution to task 5
 def get_probability(frequency_data, color):
     num_of_color = frequency_data[color.upper()]
     total_num_of_colors = sum(frequency_data.values())
@@ -69,8 +81,91 @@ def get_probability(frequency_data, color):
     return probability_of_color
 
 
-def save_color_data():
-    pass
+# Solution to task 6
+def save_color_data(frequency_data):
+    try:
+        params = config()
+        print("Connecting to the PostgreSQL database...")
+        conn = psycopg2.connect(**params)
+
+        cur = conn.cursor()
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS colors (
+            _id INTEGER PRIMARY KEY,
+            name VARCHAR ( 50 ) UNIQUE NOT NULL,
+            frequency INTEGER NOT NULL
+            );
+            """
+        )
+
+        for id, color in enumerate(frequency_data, 1):
+            color_name = color
+            color_frequency = str(frequency_data[color])
+            cur.execute(
+                f"INSERT INTO colors(_id, name, frequency) VALUES('{id}', '{color_name}', '{color_frequency}');"
+            )
+
+        cur.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print("Database connection closed.")
+
+
+# Solution to task 7
+def binary_search(arr, low, high, x):
+    if high >= low:
+        mid = (high + low) // 2
+
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+        else:
+            return binary_search(arr, mid + 1, high, x)
+    else:
+        return -1
+
+
+# Solution to task 8
+def convert_to_decimal(binary_value):
+    decimal_value = 0
+    value_arr = list(binary_value)
+    for i, value in enumerate(value_arr, 1):
+        power = len(value_arr) - i
+        decimal_value += int(value) * math.pow(2, power)
+    return int(decimal_value)
+
+
+def generate_random_binary_value(digits):
+    binary_value = ""
+    for i in range(digits):
+        digit = random.randint(0, 1)
+        binary_value += str(digit)
+    return binary_value
+
+
+def task_eight_solution(num_of_digits):
+    rand_binary_value = generate_random_binary_value(num_of_digits)
+    rand_decimal_equivalent = convert_to_decimal(rand_binary_value)
+    return rand_decimal_equivalent
+
+
+# Solution to task 9
+def fibonacci_sum(num):
+    if num < 0:
+        print("Invaid Value")
+    elif num == 0:
+        return 0
+    elif num == 1 or num == 2:
+        return 1
+    else:
+        return fibonacci_sum(num - 1) + fibonacci_sum(num - 2)
+
 
 color_array = extract_colors()
 color_data = process_colors(color_array)
@@ -82,3 +177,7 @@ color_frequency_data = create_color_dictionary(color_data)
 # print(get_median_color(color_data))
 # print(get_variance(color_frequency_data))
 # print(get_probability(color_frequency_data, "red"))
+# save_color_data(color_frequency_data)
+# print(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0, 10, 6))
+# print(task_eight_solution(4))
+# print(fibonacci_sum(50))
